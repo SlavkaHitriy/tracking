9e073aa620180612bb9ed6724c0c7479f208ba33
Viacheslav Nykytiuk <viacheslav_nykytiuk@epam.com>
2022-12-22 21:51:40 +0200

    added esling config

--- src/core/modules/GlobalTracking/GlobalTracking.js
+++ src/core/modules/GlobalTracking/GlobalTracking.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import mapboxgl from 'mapbox-gl'
+import mapboxgl from '!mapbox-gl'
 
 // Styles
 import styles from './index.module.scss'
